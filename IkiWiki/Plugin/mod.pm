8b49a8237242329ed8ee445f8fb33e26d3200ea4
Amitai Schlair <schmonz-web-ikiwiki@schmonz.com>
2013-02-14 22:12:56 -0500

    First cut, best guess, not tested.

---
+++ IkiWiki/Plugin/mod.pm
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+# Mark-Jason Dominus's MOD (Modified POD) as used for
+# [Higher-Order Perl](http://hop.perl.plover.com/book/)
+package IkiWiki::Plugin::mod;
+
+use warnings;
+use strict;
+use IkiWiki 3.00;
+# use Encode;
+
+sub import {
+	hook(type => "getsetup", id => "mod", call => \&getsetup);
+	hook(type => "htmlize", id => "mod", call => \&htmlize, longname => "MOD");
+}
+
+sub getsetup () {
+	return
+		plugin => {
+			safe => 1,
+			rebuild => 1, # format plugin
+			section => "format",
+		},
+}
+
+sub htmlize (@) {
+	my %params=@_;
+	my $content = decode_utf8(encode_utf8($params{content}));
+	return $content if $@;
+
+	eval q{use Mod::HTML};
+	return $content if $@;
+
+	my $driver = Mod::HTML->new('text')
+		or die "Couldn't create driver for text: $!; aborting.\n";
+	$driver->setparam('startchapter', 1);
+	return $driver->do_text($content);
+}
+
+1
